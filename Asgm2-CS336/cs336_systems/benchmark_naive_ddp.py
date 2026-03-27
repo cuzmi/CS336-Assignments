2a536b1a6e0015987a040724e837953c7b9c2789
Cuzmi <simon.weijie@gmail.com>
2026-03-27 10:24:06 +0800

    benchmarking naive ddp

--- Asgm2-CS336/cs336_systems/benchmark_naive_ddp.py
+++ Asgm2-CS336/cs336_systems/benchmark_naive_ddp.py
@@ -7,19 +7,13 @@
 import torch
 import time
 import os
-import torch.nn as nn
+
+import torch.nn.functional as f
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.optim import AdamW
 
-from torch.utils.data import Dataset, DataLoader
 from cs336_basics import model
-
-
-world_size = 2
-device_type = 'gpu'
-
-
 
 vocab_size = 10000
 d_model = 1600
@@ -47,6 +41,7 @@ def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12900'
     if torch.cuda.is_available():
+        torch.cuda.set_device(rank)
         dist.init_process_group(
             backend = 'nccl',
             world_size = world_size,
@@ -73,7 +68,7 @@ def get_train_batch(rank, world_size, vocab_size, batch_size, context_length, de
 
     x_local = torch.empty(
         (local_bs, context_length),
-        device = device, # TODO: 这里的device = device不是多余吗，因为本来就在当前rank上使用empty生成 - [By: Weijie] - 2026/03/26
+        device = device,
         dtype = torch.long
     )
     y_local = torch.empty(
@@ -100,13 +95,47 @@ def get_train_batch(rank, world_size, vocab_size, batch_size, context_length, de
 
     return x_local, y_local
 
+def train_one_step(LM, x, y, optimizer, device, world_size) -> float: 
+    # NOTE: optimzier 清空模型上的grad - [By: Weijie] - 2026/03/27
+    optimizer.zero_grad(set_to_none = True)
+
+    if device.type == 'cuda':
+        torch.cuda.synchronize(device)
+    
+    # x - local_bs, context_length; y, local_bs, context_length； logits local_bs, context_length, vocab_size
+    logits = LM(x)
+    loss = f.cross_entropy(
+        logits.reshape(-1, logits.shape[-1]),
+        y.reshape(-1)
+    )
+    loss.backward()
+
+    # benchmarking
+    if device.type == 'cuda':
+        torch.cuda.synchronize(device)
+    ar_start = time.perf_counter()
+
+    for p in LM.parameters():
+        if p.grad is None:
+            continue
+        dist.all_reduce(p.grad, op = dist.ReduceOp.SUM)
+        p.grad.div_(world_size)
+    
+    if device.type == 'cuda':
+        torch.cuda.synchronize(device)
+    ar_end = time.perf_counter()
+    
+    optimizer.step()
+
+    return ar_end - ar_start
+
 # train_main 是用来做作为主内容训练的
 # setup - batch 生成分配 + model + optimzier 建立 - 训练 - grad reduce
-def train_main(rank, world_size, vocab_size):
+def train_main(rank, world_size, vocab_size, batch_size, context_length, warmup):
     device = setup(rank, world_size)
 
-    # 生成数据集 ~ 可以一起实现
-    x, y = get_train_batch()
+    # 生成数据集 ~ 每个进程都分配到一份
+    x, y = get_train_batch(rank, world_size, vocab_size, batch_size, context_length, device)
 
 
     LM = model.BasicsTransformerLM(
@@ -116,24 +145,48 @@ def train_main(rank, world_size, vocab_size):
         num_layers,
         num_heads,
         d_ff,
-        rope_theta
-    )
+        rope_theta,
+    ).to(device)
 
-    optimizer = AdamW(params = LM.parameters, lr = 1e-4, betas = [0.99,0.999])
+    # NOTE: optimzier本身不是数据，不是包含很多内容的对象，而是一种作用方法，不用to device - [By: Weijie] - 2026/03/27
+    optimizer = AdamW(params = LM.parameters(), lr = 1e-4, betas = (0.99,0.999))
 
-    # 单步训练
+    LM.train()
+    # NOTE: warmup就是走几遍要记时的全流程 - [By: Weijie] - 2026/03/27
+    for _ in range(warmup):
+        _ = train_one_step(LM, x, y, optimizer, device, world_size)
 
-    return
+    # 正式训练和benchmark
+    for _ in range(2):
+        if device.type == 'cuda':
+            torch.cuda.synchronize(device)
+        start = time.perf_counter()
+
+        all_reduce_time = train_one_step(LM, x, y, optimizer, device, world_size)
+        
+        if device.type == 'cuda':
+            torch.cuda.synchronize(device)
+        end = time.perf_counter()
+
+        train_time = end - start
+        ratio = all_reduce_time / train_time
+
+        if rank == 0:
+            print(
+                f"[rank {rank}] "
+                f"train_one_step={train_time:.6f}s, "
+                f"all_reduce_time={all_reduce_time:.6f}s, "
+                f"ratio={ratio * 100:.3f}%"
+            )
+
+    dist.destroy_process_group()
 
 if __name__ == "__main__":
 
-    mp.spawn(fn = train_main, args = (world_size, device_type), npcrocs = world_size, join = True)
+    world_size = 2
+    warmup = 2
+
+    cfg = (world_size, vocab_size, batch_size, context_length, warmup)
+
+    mp.spawn(fn = train_main, args = cfg, nprocs = world_size, join = True)
     
-
-
-    # ~ warmup + sy -> for train measurement
-
-    # forward 
-
-
-    # backward - ddp
