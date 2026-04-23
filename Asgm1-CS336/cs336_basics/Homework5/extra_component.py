7ff57c2837a3d3e9855a0c03bf8db945c6707f6f
Cuzmi <simon.weijie@gmail.com>
2026-04-23 15:50:46 +0800

    checkpoint done

--- Asgm1-CS336/cs336_basics/Homework5/extra_component.py
+++ Asgm1-CS336/cs336_basics/Homework5/extra_component.py
@@ -1,7 +1,10 @@
 """
 add data_loader, checkpoint, training loop
 """
+from collections import defaultdict
+
 import torch
+import torch.nn as nn
 import numpy as np
 from typing import Tuple
 
@@ -15,3 +18,19 @@ def get_batch(dataset, batch_size, context_length, device) -> Tuple[torch.Tensor
     y = torch.stack([torch.tensor(dataset[i + 1: i + context_length + 1], dtype = torch.long) for i in indices], dim = 0).to(device)
 
     return (x, y)
+
+def save_checkpoint(model: nn.Module, optimizer: torch.optim.Optimizer, iteration: int, out: str):
+    all_dict = {}
+    all_dict['model_dict'] = model.state_dict()
+    all_dict['optim_dict'] = optimizer.state_dict()
+    all_dict['iteration'] = iteration
+
+    torch.save(all_dict, out)
+
+def load_checkpoint(src: str, model: nn.Module, optimizer: torch.optim.Optimizer) -> int:
+    all_dict = torch.load(src)
+
+    model.load_state_dict(all_dict["model_dict"])
+    optimizer.load_state_dict(all_dict["optim_dict"])
+
+    return all_dict["iteration"]
